54995a1f7c864aefdef59d4d9115297cfc406b2a
sachinbondar145 <72794862+sachinbondar145@users.noreply.github.com>
2021-02-02 12:15:44 +0530

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
             style: GoogleFonts.lato(
                 textStyle: Theme.of(context).textTheme.display1,
                 color: Colors.white,
-                fontSize: 18),
+                fontSize: 14),
           ))),
       backgroundColor: Color(0xFF0E0F1B),
       body: Container(
