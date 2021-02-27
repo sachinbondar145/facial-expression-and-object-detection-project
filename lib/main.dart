d16ba803f3667f0d73de0eb6f48d404707cf9c90
sachinbondar145 <72794862+sachinbondar145@users.noreply.github.com>
2021-02-27 22:27:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:snap/HomePage.dart';
 import 'package:snap/StartPage.dart';
+import 'package:snap/photofilter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Snap', 
-      color: Colors.orange,
+      title: 'Snappy',
+      color: Colors.blue,
       theme: ThemeData.dark(),
       home: Home(),
     );
   }
 }
 
-// Face Recognisation
